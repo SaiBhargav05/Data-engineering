89593f60c7fe1bf1019407a25bfcd85ab27b72f6
SaiBhargav05 <saibhargav052000@gmail.com>
2026-02-19 19:35:45 -0500

    added logic to read table_list

--- config.py
+++ config.py
@@ -4,16 +4,14 @@ DB_DETAILS = {
     'dev': {
         'SOURCE_DB': {
             'DB_TYPE': 'mysql',
-            'DB_HOST': '139.99.209.131',
-            #'DB_PORT': '3306', # Added Port
+            'DB_HOST': '127.0.0.1',
             'DB_NAME': 'retail_db',
             'DB_USER': os.environ.get('SOURCE_DB_USER'),
             'DB_PASS': os.environ.get('SOURCE_DB_PASS')
         },
         'TARGET_DB': {
             'DB_TYPE': 'postgres',
-            'DB_HOST': '139.99.209.131',
-            #'DB_PORT': '5432', # Added Port
+            'DB_HOST': '127.0.0.1',
             'DB_NAME': 'retail_db',
             'DB_USER': os.environ.get('TARGET_DB_USER'),
             'DB_PASS': os.environ.get('TARGET_DB_PASS')
